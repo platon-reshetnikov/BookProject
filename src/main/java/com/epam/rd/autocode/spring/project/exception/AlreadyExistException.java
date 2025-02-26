8eaef716b37637c541dc7236b0f285ddd49ec5de
platon-reshetnikov <82944686+platon-reshetnikov@users.noreply.github.com>
2025-02-26 21:44:57 +0200

    Exception imle

--- src/main/java/com/epam/rd/autocode/spring/project/exception/AlreadyExistException.java
+++ src/main/java/com/epam/rd/autocode/spring/project/exception/AlreadyExistException.java
@@ -1,5 +1,7 @@
 package com.epam.rd.autocode.spring.project.exception;
 
-public class AlreadyExistException {
-    // TODO Place your code here
+public class AlreadyExistException extends RuntimeException {
+    public AlreadyExistException(String message){
+        super(message);
+    }
 }
--- src/main/java/com/epam/rd/autocode/spring/project/exception/NotFoundException.java
+++ src/main/java/com/epam/rd/autocode/spring/project/exception/NotFoundException.java
@@ -1,5 +1,7 @@
 package com.epam.rd.autocode.spring.project.exception;
 
-public class NotFoundException{
-    // TODO Place your code here
+public class NotFoundException extends RuntimeException{
+    public NotFoundException(String message){
+        super(message);
+    }
 }
