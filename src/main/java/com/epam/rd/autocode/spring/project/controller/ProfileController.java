a0b2b11b9cc5731a2c49d8beb731f6167721d1bc
platon-reshetnikov <82944686+platon-reshetnikov@users.noreply.github.com>
2025-03-02 21:45:42 +0200

    Fix For Any Registered Users

--- src/main/java/com/epam/rd/autocode/spring/project/controller/ProfileController.java
+++ src/main/java/com/epam/rd/autocode/spring/project/controller/ProfileController.java
@@ -5,6 +5,7 @@ import com.epam.rd.autocode.spring.project.dto.EmployeeDTO;
 import com.epam.rd.autocode.spring.project.model.Client;
 import com.epam.rd.autocode.spring.project.model.Employee;
 import com.epam.rd.autocode.spring.project.service.impl.UserServiceImpl;
+import jakarta.validation.Valid;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -26,70 +27,83 @@ public class ProfileController {
     @GetMapping
     public String viewProfile(Model model) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String email = authentication.getName(); // Get the email of the logged-in user
+        String email = authentication.getName();
 
-        // Fetch user details based on role
         if (authentication.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("CLIENT"))) {
             Client client = userService.getClientByEmail(email);
             model.addAttribute("user", client);
+            model.addAttribute("userType", "client");
         } else if (authentication.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("EMPLOYEE"))) {
             Employee employee = userService.getEmployeeByEmail(email);
             model.addAttribute("user", employee);
+            model.addAttribute("userType", "employee");
         }
 
-        return "profile"; // Thymeleaf template for viewing profile
+        return "profile";
     }
 
     // Edit profile form
     @GetMapping("/edit")
     public String editProfileForm(Model model) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String email = authentication.getName(); // Get the email of the logged-in user
+        String email = authentication.getName();
 
-        // Fetch user details based on role
         if (authentication.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("CLIENT"))) {
             Client client = userService.getClientByEmail(email);
-            model.addAttribute("user", new ClientDTO(client.getEmail(), client.getPassword(), client.getName(), client.getBalance()));
+            ClientDTO clientDTO = new ClientDTO(client.getEmail(), client.getPassword(), client.getName(), client.getBalance());
+            model.addAttribute("client", clientDTO);
+            model.addAttribute("userType", "client");
         } else if (authentication.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("EMPLOYEE"))) {
             Employee employee = userService.getEmployeeByEmail(email);
-            EmployeeDTO employeeDTO = EmployeeDTO.builder()
-                    .email(employee.getEmail())
-                    .password(employee.getPassword())
-                    .name(employee.getName())
-                    .phone(employee.getPhone())
-                    .birthDate(employee.getBirthDate())
-                    .build();
-            model.addAttribute("user", employeeDTO);
+            EmployeeDTO employeeDTO = new EmployeeDTO(employee.getEmail(), employee.getPassword(), employee.getName(), employee.getPhone(), employee.getBirthDate());
+            model.addAttribute("employee", employeeDTO);
+            model.addAttribute("userType", "employee");
         }
-        return "edit-profile"; // Thymeleaf template for editing profile
+        return "edit-profile";
     }
 
-    // Save edited profile
+    // Save edited profile for Client
     @PostMapping("/edit")
-    public String saveProfile(
-            @RequestParam("type") String type,
-            @ModelAttribute("user") Object userDTO,
-            BindingResult bindingResult) {
-
+    public String saveClientProfile(
+            @RequestParam("userType") String userType,
+            @Valid @ModelAttribute("client") ClientDTO clientDTO,
+            BindingResult bindingResultClient,
+            @Valid @ModelAttribute("employee") EmployeeDTO employeeDTO,
+            BindingResult bindingResultEmployee,
+            Model model) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication == null || !authentication.isAuthenticated()) {
-            return "redirect:/login"; // Redirect to login if not authenticated
+            return "redirect:/login";
         }
 
         String email = authentication.getName();
-        System.out.println("Received DTO: " + userDTO);
 
-        if (bindingResult.hasErrors()) {
-            System.out.println("Validation errors: " + bindingResult.getAllErrors());
-            return "edit-profile"; // Return to form if validation fails
+        if ("client".equals(userType)) {
+            if (bindingResultClient.hasErrors()) {
+                model.addAttribute("userType", "client");
+                return "edit-profile";
+            }
+            try {
+                userService.updateClient(email, clientDTO);
+            } catch (Exception e) {
+                model.addAttribute("error", e.getMessage());
+                model.addAttribute("userType", "client");
+                return "edit-profile";
+            }
+        } else if ("employee".equals(userType)) {
+            if (bindingResultEmployee.hasErrors()) {
+                model.addAttribute("userType", "employee");
+                return "edit-profile";
+            }
+            try {
+                userService.updateEmployee(email, employeeDTO);
+            } catch (Exception e) {
+                model.addAttribute("error", e.getMessage());
+                model.addAttribute("userType", "employee");
+                return "edit-profile";
+            }
         }
 
-        if ("client".equals(type) && userDTO instanceof ClientDTO clientDTO) {
-            userService.updateClient(email, clientDTO);
-        } else if ("employee".equals(type) && userDTO instanceof EmployeeDTO employeeDTO) {
-            userService.updateEmployee(email, employeeDTO);
-        }
-
-        return "redirect:/profile"; // Redirect after saving
+        return "redirect:/profile";
     }
 }
